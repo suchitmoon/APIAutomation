6d0fa43db78d37e67e3fb8629f39996a9bcaa169
Suchit <suchitmoon@gmail.com>
2026-08-25 01:00:46 +0530

    new file added

---
+++ src/test/java/userManagement/headers.java
@@ -0,0 +1,43 @@
+package userManagement;
+
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+import org.testng.annotations.Test;
+
+import io.restassured.http.Header;
+import io.restassured.http.Headers;
+import io.restassured.internal.http.HttpResponseDecorator.HeadersDecorator;
+import io.restassured.response.Response;
+
+public class headers {
+    
+
+        @Test
+        public void test1(){
+
+           Response response = given()
+           .when()
+           .get("https://reqres.in/api/users?page=2")
+           .then()
+           .extract().response();
+   
+                Headers headers =response.getHeaders();
+
+                for(Header h: headers){
+                System.out.println(h.getName() + " : "+ h.getValue());
+              }
+               
+              
+                for(Header h: headers){
+                    if( h.getName().contains("Server") ){
+              
+                    System.out.println(h.getName() + " : "+ h.getValue());
+
+                    assertEquals(h.getValue(), "cloudflare");
+              }
+                }
+   
+        }
+
+}
